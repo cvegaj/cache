c47d7aa78f26e769c4bb2b26bed9a22a451bff5e
cvegaj <carlavj93@gmail.com>
2016-09-25 17:16:14 -0600

    rev3

--- main.cpp
+++ main.cpp
@@ -1,32 +1,73 @@
 #include <stdlib.h>
 #include <iostream>
-
+#include <vector>
+#include <math.h>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
+struct block{
+  bool bValidBit = false;
+  long lBlockTag = 0;
+};
+
 class cache{
-
   private:
-    int iTagSize = 0;
-    int iIndexSize = 0;
-    int iOffsetSize = 0;
-    unsigned long ulAddress = 0;
-
+    int iTagSize, iIndexSize;
+    int iNumSets, iRand, iNumBlocks, iOffsetSize;
+    long lAddress, lIndex, lTag, lIndexMask;
+    std::vector<std::vector<block> > vBlocks;
   public:
-    long lBlockSize;
+    long long llBlockSize;
     int iAssociativity;
-    long lCacheSize;
-
+    long long llCacheSize;
+    void splitAddress();
+    bool readAddress(long);
     //Constructor con 3 parametros
-    cache(int iAssoc,long lCSize,long lBSize){
-      lBlockSize = lBSize;
+    cache(int iAssoc,long long llCSize,long long llBSize){
+      llBlockSize = llBSize;
       iAssociativity = iAssoc;
-      lCacheSize = lCSize;
+      llCacheSize = llCSize;
+      iNumBlocks = llCSize/llBSize;
+      iNumSets = iNumBlocks/iAssoc;
+      iOffsetSize = int(log2(llBSize));
+      iIndexSize = int(log2(iNumSets));
+      iTagSize = 32 - iIndexSize - iOffsetSize;
+      vBlocks.resize(iNumSets, std::vector<block>(iAssociativity) );
     }
     //Destructor >:O
     ~cache(){}
-
 };
+
+//para dividir la direccion en tag, index y offset
+void cache::splitAddress(){
+  for(int i = 0; i < iIndexSize; i++){
+    lIndexMask <<= 1; //se crea una mascara para index
+    lIndexMask |= 1; //indexMask or 1
+  }
+  lIndex = lAddress >> (iOffsetSize);
+  lIndex = lIndex & lIndexMask;
+  lTag = lAddress >> (iOffsetSize+iIndexSize);
+}
+
+//despues de saber en que set estoy de acuerdo al index
+bool cache::readAddress(long lAddr){
+  lAddress = lAddr;
+  splitAddress();
+  for(int i=0; i<iAssociativity; i++){ //para buscar la cantidad de bloques
+    if (vBlocks[lIndex][i].bValidBit){
+      if (vBlocks[lIndex][i].lBlockTag == lTag){
+        return true;
+      }
+    }
+  }
+  iRand = rand() % iAssociativity;
+  vBlocks[lIndex][iRand].bValidBit = true;
+  vBlocks[lIndex][iRand].lBlockTag = lTag;
+  return false;
+}
 
 int main(int arcg, char* argv[]){
 
@@ -36,13 +77,28 @@ int main(int arcg, char* argv[]){
   llParameter[2]=atoll(argv[3]);
 
   ifstream datos("aligned.trace");
-	string sLine;
-	unsigned long ulAddress;
+	long lAddr;
 	bool bHit;
 	long long llTotal = 0;
 	long long llHits = 0;
 	long long llMisses = 0;
 	double dMissRate;
+  string line;
+  cache myCache(llParameter[0],llParameter[1],llParameter[2]);
 
-
+  while ( getline (datos,line) ) {
+    getline(datos,line);
+    line.erase(line.begin()+8,line.end());
+    istringstream(line) >> std::hex >> lAddr;
+  	bHit = myCache.readAddress(lAddr);
+    if(bHit){
+      llHits++;
+    }
+    llTotal++;
+  }
+  cout << "Hits: " << llHits << endl;
+  cout << "Total: " << llTotal << endl;
+  llMisses = llTotal - llHits;
+  dMissRate = (double(llMisses)/double(llTotal))*100;
+  cout << "Miss Rate: " << dMissRate << endl;
 }
