5da8063687542d9ad5ebf32996d43b7c740bd707
cvegaj <carlavj93@gmail.com>
2016-09-24 18:57:29 -0600

    first main revision

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <iostream>
+
+
+using namespace std;
+
+int main(int arcg, char* argv[]){
+
+  bool (int arcg,)
+  long long llParameter[3];
+  llParameter[0]=atoll(argv[1]);
+  llParameter[1]=atoll(argv[2]);
+  llParameter[2]=atoll(argv[3]);
+
+  ifstream datos("aligned.trace");
+	string sLine;
+	unsigned long ulAddress;
+	bool bHit;
+	long long llTotal = 0;
+	long long llHits = 0;
+	long long llMisses = 0;
+	double dMissRate;
+
+
+}
